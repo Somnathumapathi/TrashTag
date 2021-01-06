cdc69326fdf38333bb2f6ecf5b215703c8ab667c
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2021-01-06 18:21:46 +0530

    BaseScaffolds

---
+++ TrashTagBackend/run.py
@@ -0,0 +1,8 @@
+from TrashTagBackend import create_app
+from TrashTagBackend.config import Config
+
+app = create_app()
+config = Config()
+if __name__ == '__main__':
+	#Runs on localhost:8080
+	app.run(debug=not config.PRODUCTION_MODE, host=config.HOST_NAME, port=config.PORT_NUMBER)
